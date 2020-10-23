99a46c8d59a6ee6789f2bd8840bcc2654bbea11a
IlyaOvch <72275117+IlyaOvch@users.noreply.github.com>
2020-10-23 17:20:41 +0300

    Add files via upload

---
+++ Ovcharenko_GIBO_01_18_Lab2/src/Ovcharenko/lab2/TestAuthor.java
@@ -0,0 +1,8 @@
+package Ovcharenko.lab2;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author a1 = new Author("Ovcharnko Ilya Sergeevich", "Ovcharnko@mail.ru", 'M');
+        System.out.println(a1);
+    }
+}
